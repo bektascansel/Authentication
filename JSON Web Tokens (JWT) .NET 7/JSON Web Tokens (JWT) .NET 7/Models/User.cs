56595e3f6b187744e271af18ceca959d93f0c1aa
bektascansel <cansel.bektas6447@gmail.com>
2024-02-04 22:04:02 +0300

    JWT

---
+++ JSON Web Tokens (JWT) .NET 7/JSON Web Tokens (JWT) .NET 7/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace JSON_Web_Tokens__JWT__.NET_7.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+    }
+}
---
+++ JSON Web Tokens (JWT) .NET 7/JSON Web Tokens (JWT) .NET 7/Models/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace JSON_Web_Tokens__JWT__.NET_7.Models
+{
+    public class UserDto
+    {
+        public required string  Username { get; set; }
+        public required string Password { get; set; }
+
+
+    }
+}
